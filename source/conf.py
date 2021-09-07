920ca983879fcf5cc782789a54d076a074f417d0
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-09-07 13:15:26 +0200

    fix timeout, add to the index

--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,7 @@ release = ''
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
-
+nbsphinx_timeout = 400
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
